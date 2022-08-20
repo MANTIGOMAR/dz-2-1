3de78744233250c628185438c61e9ee8efc60ca5
Aleksandr Mikhailov <dmitrijdmitriev@gmail.com>
2022-08-20 18:59:15 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price =1445;
+        int miles = price / 20;
+        int Bonus = miles;
+        System.out.println(Bonus);
+    }
+}
