34891079651a426c4131bf8119dbbd4921db7339
Aleksandr Mikhailov <dmitrijdmitriev@gmail.com>
2022-08-24 19:56:44 +0300

    rm out

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int price =1445;
+        int price = 1445;
         int miles = price / 20;
         int Bonus = miles;
         System.out.println(Bonus);
