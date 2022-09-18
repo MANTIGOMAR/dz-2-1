679bd7ae44916279ec3315233bbbb5e00ceee7c8
Aleksandr Mikhailov <dmitrijdmitriev@gmail.com>
2022-09-18 21:05:45 +0300

    modified file

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 1445;
         int miles = price / 20;
-        int Bonus = miles;
-        System.out.println(Bonus);
+        int bonus = miles;
+        System.out.println(bonus);
     }
 }
